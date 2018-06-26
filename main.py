a1386091c276d01fc7aa6c430357a79be96ed1db
Stan Ovchinnikov <lishnih@gmail.com>
2018-06-26 23:59:05 +0400

    Initial version.

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Stan 2016-04-24
+
+from __future__ import (division, absolute_import,
+                        print_function, unicode_literals)
+
+from tkprocess.tkprocess import main
+
+
+if __name__ == '__main__':
+    main()
